8ef2168f4966b241daeef4ff22fcb599b8dcf521
Sol <mperezfreire@frba.utn.edu.ar>
2024-12-05 02:00:27 -0300

    add users array

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import express from 'express';
 
 const app = express();
 app.listen(3000, () => {
-    console.log(`listening on port 3000`)
+    console.log(`listening on port 3000`);
 })
---
+++ src/db/users.js
@@ -0,0 +1,66 @@
+export const users = [
+    {
+        name: 'Homero J',
+        surname: 'Simpson',
+        edad: 38,
+        email: 'homerosimpson@gmail.com',
+        direccion: 'Avenida Siempreviva 742',
+        localidad: 'Springfield',
+        provincia: 'Oregon'
+    },
+    {
+        name: 'Mirtha',
+        surname: 'Legrand',
+        edad: 15987516,
+        email: 'mirthalegrand@gmail.com',
+        direccion: 'Av Libertador 1546',
+        localidad: 'CABA',
+        provincia: 'CABA'
+    },
+    {
+        name: 'P',
+        surname: 'Sherman',
+        edad: 32,
+        email: 'psherman@gmail.com',
+        direccion: 'Wallaby 42',
+        localidad: 'Sydney',
+        provincia: 'Nueva Gales del Sur'
+    },
+    {
+        name: 'Suki',
+        surname: 'Chat',
+        edad: 4,
+        email: 'suki@gmail.com',
+        direccion: 'Lexington 12',
+        localidad: 'London',
+        provincia: 'London'
+    },
+    {
+        name: 'Milka',
+        surname: 'Chat',
+        edad: 3,
+        email: 'milka@gmail.com',
+        direccion: '652 5th avenue',
+        localidad: 'Manhattan',
+        provincia: 'New York'
+    },
+    {
+        name: 'Merlina',
+        surname: 'Chat',
+        edad: 9,
+        email: 'merlina@gmail.com',
+        direccion: 'Av des Champs-Elysees 52',
+        localidad: 'Paris',
+        provincia: 'Ile-de-France'
+    },
+    {
+        name: 'Balu',
+        surname: 'Chien',
+        edad: 11,
+        email: 'balu@gmail.com',
+        direccion: 'Gran Via 17',
+        localidad: 'Madrid',
+        provincia: 'Madrid'
+    },
+
+]
