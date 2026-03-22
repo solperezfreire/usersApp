4a58babd35443146489bbbed6aadf6601f829320
Sol <mperezfreire@frba.utn.edu.ar>
2026-03-22 01:58:21 -0300

    modify folder structure

--- src/middlewares/errorHandler.js
+++
@@ -1,6 +0,0 @@
-export const errorHandler = (err, req, res, next) => {
-    res.status(err.status || 500).json({
-        status: false,
-        message: err.message || 'Internal Server Error'
-    });
-};
